3686c1f27c2cf5409ef0f0e7154ffb098934f761
hierischmarkus <58728742+hierischmarkus@users.noreply.github.com>
2023-04-22 18:07:02 +0200

    bug fixing

---
+++ changetabs.js
@@ -0,0 +1,26 @@
+// tabs für lebenslauf
+let tablinks = document.getElementsByClassName('tab-links')
+let tabcontents = document.getElementsByClassName('tab-contents')
+
+function opentab(tabname){
+    for(let tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(let tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add('active-tab');
+}
+
+//sidemenu
+let sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
